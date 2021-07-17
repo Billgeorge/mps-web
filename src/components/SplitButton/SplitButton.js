44a435ffbbcd071dcf4f31e911cd213615bc0952
Leonardo Espinosa <jorjek4@hotmail.com>
2021-07-16 19:01:10 -0500

    MPS-adding multibutton

---
+++ src/components/SplitButton/SplitButton.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Link from '@material-ui/core/Link';
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    popperCustom: {
+        zIndex: 3            
+    },
+    paperCustom: {
+        backgroundColor:'#303f9f',
+        color:'white'                
+    },
+    menuItemCustom:{
+        '&:hover':{
+            backgroundColor:'#46529a'
+        }
+    }
+  }));
+
+
+export default function SplitButton(props) {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(null);
+  const history = useHistory();
+
+  const classes = useStyles();
+
+  const [options, setOptions] = React.useState(props.options);
+
+  const handleClick = () => {
+    console.info(`You clicked ${options[selectedIndex]}`);
+  };
+
+  const handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+    history.push(options[index].action)    
+    setOpen(false);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Grid container direction="column" alignItems="center">
+      <Grid item xs={12}>
+        <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button">
+          <Button onClick={handleClick}>Acción</Button>
+          <Button
+            color="primary"
+            size="small"
+            aria-controls={open ? 'split-button-menu' : undefined}
+            aria-expanded={open ? 'true' : undefined}
+            aria-label="select merge strategy"
+            aria-haspopup="menu"
+            onClick={handleToggle}
+          >
+            <ArrowDropDownIcon />
+          </Button>
+        </ButtonGroup>
+        <Popper className={classes.popperCustom} open={open} anchorEl={anchorRef.current} role={undefined} transition>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+              }}
+            >
+              <Paper className={classes.paperCustom} elevation={3}>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList id="split-button-menu">
+                  {options.map((option, index) => (
+                         <MenuItem
+                         className={classes.menuItemCustom}
+                         containerElement={<Link to={option.action} />}
+                         key={option}
+                         disabled={index === 2}
+                         selected={index === selectedIndex}
+                         onClick={(event) => handleMenuItemClick(event, index)}
+                        >
+                          {option.label}
+                        </MenuItem>
+                    ))}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>        
+      </Grid>
+    </Grid>
+  );
+}
--- src/views/Product/ProductDropBoard.js
+++ src/views/Product/ProductDropBoard.js
@@ -20,119 +20,125 @@ import Paper from '@material-ui/core/Paper';
 import Footer from "components/Footer/Footer.js";
 import { getMerchantId } from 'service/AuthenticationService';
 
-import {consumeServiceGet} from 'service/ConsumeService'
-import {CORE_BASEURL} from 'constant/index'
+import { consumeServiceGet } from 'service/ConsumeService'
+import { CORE_BASEURL } from 'constant/index'
 import ResponsiveDrawe from "components/LeftMenu/ResponsiveDrawer.js"
+import SplitButton from 'components/SplitButton/SplitButton';
 
 const useStyles = makeStyles(styles);
 
 
-  
-  export default function ProductDropBoard() {
 
-    const [products, setProductsl] = React.useState([]);
-    const [errorMessage, setErrorMessage] = React.useState("");    
+export default function ProductDropBoard() {
 
-    React.useEffect(() => getProductsForMerchant(), []);    
+  const [products, setProductsl] = React.useState([]);
+  const [errorMessage, setErrorMessage] = React.useState("");
 
-    const callBackSuccess = (products) =>{
-      setProductsl(products)
-      if(products.length==0){
-          setErrorMessage("No hay productos para mostrar")
-      }      
+  React.useEffect(() => getProductsForMerchant(), []);
+
+  const callBackSuccess = (products) => {
+    setProductsl(products)
+    if (products.length == 0) {
+      setErrorMessage("No hay productos para mostrar")
     }
+  }
 
-    const copyUrl = (id) => {
-      var getUrl = window.location;
-      var baseUrl = getUrl .protocol + "//" + getUrl.host + "/";
-      navigator.clipboard.writeText(baseUrl+"checkout?idc="+id);
+  const copyUrl = (id) => {
+    var getUrl = window.location;
+    var baseUrl = getUrl.protocol + "//" + getUrl.host + "/";
+    navigator.clipboard.writeText(baseUrl + "checkout?idc=" + id);
+  }
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  })
+
+  const callBack = (msg) => {
+    setProductsl([])
+    if (msg == 404) {
+      setErrorMessage("No hay productos para mostrar")
+    } else {
+      setErrorMessage("Error Cargando productos")
     }
+  }
 
-    const formatter = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2
-    })
+  const getProductsForMerchant = () => {
+    const merchantId = getMerchantId()
+    console.log('getting drop products ')
+    let url = `${CORE_BASEURL}/dropshippingsale/sellermerchant/${merchantId}`
+    consumeServiceGet(callBack, callBackSuccess, url)
+  }
 
-    const callBack = (msg) => {
-      setProductsl([])
-      if(msg==404){
-        setErrorMessage("No hay productos para mostrar")        
-      }else{
-        setErrorMessage("Error Cargando productos")
-      }      
-    }
+  const classes = useStyles();
 
-    const getProductsForMerchant = () => {
-      const merchantId = getMerchantId()
-      console.log('getting drop products ')
-      let url=`${CORE_BASEURL}/dropshippingsale/sellermerchant/${merchantId}`
-      consumeServiceGet(callBack,callBackSuccess,url)      
-    }
-      
-    const classes = useStyles();
-    
-    return (
-        <div>       
-       
-        <ResponsiveDrawe />       
-        <div className={classes.container}>
+  return (
+    <div>
+
+      <ResponsiveDrawe />
+      <div className={classes.container}>
         <GridContainer className={classes.subContainer} justify="center" >
-         <GridItem xs={12} sm={12} md={12} className={classes.grid}>           
+          <GridItem xs={12} sm={12} md={12} className={classes.grid}>
+            <Grid container className={classes.box} spacing={3}>
+              <Grid item xs={12}><h2>Tus Productos Dropshipping</h2></Grid>
+              <GridItem xs={12} sm={12} md={12} className={classes.grid}>
                 <Grid container className={classes.box} spacing={3}>
-                <Grid item  xs={12}><h2>Tus Productos Dropshipping</h2></Grid>
-                <GridItem xs={12} sm={12} md={12} className={classes.grid}>        
-                    <Grid container className={classes.box}  spacing={3}>               
-                        <Grid item xs={12} sm={12} md={12} >
-                            A continuación ves los productos dropshiṕping que vinculaste:                     
-                        </Grid>                              
-                    </Grid>
-                </GridItem>   
-                   <Grid item xs={12} >
-                   <TableContainer component={Paper}>
-                    <Table className={classes.table} aria-label="simple table">
-                      <TableHead>
-                        <TableRow>                          
-                          <TableCell align="center">Nombre</TableCell>
-                          <TableCell align="center">Precio de compra</TableCell> 
-                          <TableCell align="center">Precio de venta</TableCell>
-                          <TableCell align="center">Inventario</TableCell>
-                          <TableCell align="center">Url checkout</TableCell>
-                          <TableCell align="center">Editar</TableCell>                         
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {products.map((row) => (
-                          <TableRow>                            
-                            <TableCell align="center">{row.name}</TableCell>
-                            <TableCell align="center">{
-                              formatter.format(row.buyPrice)
-                            }</TableCell>
-                            <TableCell align="center">{
-                              formatter.format(row.sellPrice)
-                            }</TableCell>
-                            <TableCell align="center">{row.inventory}</TableCell>
-                            <TableCell align="right"><center><Button onClick={() => copyUrl(row.id)} color="primary">Copiar Enlace</Button></center></TableCell>                            
-                            <TableCell align="right"><center><a href={"/edit-checkout?idp="+row.id}><Button color="primary">Editar</Button></a></center></TableCell>                            
+                  <Grid item xs={12} sm={12} md={12} >
+                    A continuación ves los productos dropshiṕping que vinculaste:
+                  </Grid>
+                </Grid>
+              </GridItem>
+              <Grid item xs={12} >
+                <TableContainer component={Paper}>
+                  <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell align="center">Nombre</TableCell>
+                        <TableCell align="center">Precio de compra</TableCell>
+                        <TableCell align="center">Precio de venta</TableCell>
+                        <TableCell align="center">Inventario</TableCell>
+                        <TableCell align="center">Url checkout</TableCell>
+                        <TableCell align="center">Acciones</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {products.map((row) => (
+                        <TableRow>
+                          <TableCell align="center">{row.name}</TableCell>
+                          <TableCell align="center">{
+                            formatter.format(row.buyPrice)
+                          }</TableCell>
+                          <TableCell align="center">{
+                            formatter.format(row.sellPrice)
+                          }</TableCell>
+                          <TableCell align="center">{row.inventory}</TableCell>
+                          <TableCell align="right"><center><Button onClick={() => copyUrl(row.id)} color="primary">Copiar Enlace</Button></center></TableCell>
+                          <TableCell align="center">
+                            <SplitButton options={[
+                              { label: "Editar", action: "/edit-checkout?idp=" + row.id },
+                              { label: "Crear promoción", action: "/create-promotion?idc=" + row.id }
+                            ]} ></SplitButton>
+                            </TableCell>                            
                           </TableRow>
                         ))}
                       </TableBody>
                     </Table>
                   </TableContainer>
                    </Grid>
-                </Grid>   
+              </Grid>   
          </GridItem>
-         {errorMessage != ""
-                  ?
-                  <Alert severity="error">{errorMessage}</Alert>
-                  : <span>	&nbsp;</span>   
-          }   
+            {errorMessage != ""
+              ?
+              <Alert severity="error">{errorMessage}</Alert>
+              : <span>	&nbsp;</span>
+            }   
         </GridContainer>
         </div>
         <Footer />
       </div>
-      
-      
-      
-    );
+
+
+
+      );
   }
